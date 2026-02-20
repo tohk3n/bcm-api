c0cdec8aea1e8dffdffcd8ac9eb55b53bb48c083
tohk3n <lacknir@gmail.com>
2026-02-20 10:08:11 -0600

    fix: intial archive extraction was incomplete.this is the base project.

---
+++ src/test/java/io/tohk3n/bcm/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tohk3n.bcm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
